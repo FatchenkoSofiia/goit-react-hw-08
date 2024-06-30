25c4da2690664dc01cb2b4ece6ab68cd92197153
FatchenkoSofiia <fatcenkosona@gmail.com>
2024-06-30 10:13:52 +0300

    required

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['react-modal'],
-    },
-  },
-  server: {
-    hmr: {
-      overlay: false,
-    },
-  },
+    sourcemap: true,
+  }
 });
